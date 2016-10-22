bf1c4a7ffd45e1244328d7aa38b3f2860d8bed41
Sarthak Srinivas <sarthaksrinivas@gatech.edu>
2016-10-22 17:07:20 -0400

    THE MOTOR IS ALIVEgit add .git add .

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,11 +1,30 @@
+#include <Adafruit_DRV2605.h>
+#include <Wire.h>
 
+// Instance Variables
+Adafruit_DRV2605 drv;
+uint8_t effect = 3;
+uint32_t wait = 10;
+const int motorPin = 3;
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+//
+//    drv.begin();
+//    drv.selectLibrary(1);
+//    drv.setMode(DRV2605_MODE_INTTRIG);
 
+    pinMode(motorPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    digitalWrite(motorPin, HIGH);
+    delay(1000);
+    digitalWrite(motorPin, LOW);
+    delay(1000);
+//    Serial.print("Effect #"); Serial.println(effect);
+//    drv.setWaveform(0, effect);
+//    drv.setWaveform(1, 0);
+//    drv.go();    
+    Serial.println("Cycle Finished!");
 }
