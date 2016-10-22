f9206d751959b3c6a547358c2eb5fa0bca67ac0e
Sarthak Srinivas <sarthaksrinivas@gatech.edu>
2016-10-22 01:46:20 -0400

    added functions for all the letters

--- letters/letters.ino
+++ letters/letters.ino
@@ -7,18 +7,25 @@ void setup() {
 }
 
 void loop() {
-    a();
-    space();
     // printing
     if (Serial.available() > 0) {
         int incomingByte = Serial.read();
 
+        // make everything uppercase
         if (incomingByte <= 122 && incomingByte >= 97) {
             incomingByte = incomingByte - 32;
-        }        
+        }
+
+        if (incomingByte == 65) {
+            a();
+            charspace();
+        }
         
         Serial.print("I received ");
         Serial.println(incomingByte, DEC);
+
+        incomingByte = 0;
+
     }
 }
 
@@ -41,7 +48,7 @@ void dash() {
     digitalWrite(LED_BUILTIN, LOW);
 }
 
-void next_char() {
+void charspace() {
     for (int i = 0; i < 3; i++) {
         gap();
     }
@@ -56,6 +63,272 @@ void space() {
 void a() {
     dot();
     gap();
+    
     dash();
 }
 
+void b() {
+    dash();
+    gap();
+
+    dot();
+    gap();
+
+    dot();
+    gap();
+
+    dot();
+}
+
+void c() {
+    dash();
+    gap();
+
+    dot();
+    gap();
+
+    dash();
+    gap();
+
+    dot();
+}
+
+void d() {
+    dash();
+    gap();
+
+    dot();
+    gap();
+
+    dot();    
+}
+
+void e() {
+    dot();
+}
+
+void f() {
+    dot();
+    gap();
+
+    dot();
+    gap();
+    
+    dash();
+    gap();
+
+    dot();
+}
+
+void g() {
+    dash();
+    gap();
+
+    dash();
+    gap();
+    
+    dot();
+}
+
+void h() {
+    dot();
+    gap();
+
+    dot();
+    gap();
+
+    dot();
+    gap();
+    
+    dot();
+}
+
+void i() {
+    dot();
+    gap();
+    
+    dot();
+}
+
+void j() {
+    dot();
+    gap();
+
+    dash();
+    gap();
+
+    dash();
+    gap();
+
+    dash();
+}
+
+void k() {
+    dash();
+    gap();
+
+    dot();
+    gap();
+    
+    dash();
+}
+
+void l() {
+    dot();
+    gap();
+    
+    dash();
+    gap();
+
+    dot();
+    gap();
+
+    dot();
+}
+
+void m() {
+    dash();
+    gap();
+
+    dash();
+    gap();
+}
+
+void n() {
+    dash();
+    gap();
+
+    dot();
+}
+
+void o() {
+    dash();
+    gap();
+
+    dash();
+    gap();
+
+    dash();
+}
+
+void p() {
+    dot();
+    gap();
+
+    dash();
+    gap();
+
+    dash();
+    gap();
+
+    dot();
+}
+
+void q() {
+    dash();
+    gap();
+
+    dash();
+    gap();
+
+    dot();
+    gap();
+
+    dash();
+}
+
+void r() {
+    dot();
+    gap();
+
+    dash();
+    gap();
+
+    dot();    
+}
+
+void s() {
+    dot();
+    gap();
+
+    dot();
+    gap();
+
+    dot();    
+}
+
+void t() {
+    dash();
+}
+
+void u() {
+    dot();
+    gap();
+    
+    dot();
+    gap();
+    
+    dash()
+}
+
+void v() {
+    dot();
+    gap();
+    
+    dot();
+    gap();
+    
+    dot();
+    gap();
+    
+    dash();
+}
+
+void w() {
+    dot();
+    gap();
+
+    dash();
+    gap();
+
+    dash();
+}
+
+void x() {
+    dash();
+    gap();
+    
+    dot();
+    gap();
+    
+    dot();
+    gap();
+    
+    dash();
+}
+
+void y() {
+    dash();
+    gap();
+    
+    dot();
+    gap();
+    
+    dash();
+    gap();
+    
+    dash();
+}
+
+void z() {
+    dash();
+    gap();
+    
+    dash();
+    gap();
+    
+    dot();
+    gap();
+    
+    dot()
+}
